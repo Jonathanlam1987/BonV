7ed7a0325a4671b158ffda4b0928cffbce65bb3e
jonathanlam1987 <jonathanlam@icloud.com>
2021-03-06 11:24:01 -0700

    organized css

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const expressHandlebars = require('express-handlebars');
+const app = express();
+const bodyParser = require("body-parser");
+
+
+
+app.engine(
+    "handlebars",
+    expressHandlebars({
+    }) 
+  );
+  app.set("view engine", "hbs");
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.get('/', (req, res) => {
+  res.render("main", {layout: null });
+})
+
+
+app.get('/', (req,res) => {
+  res.send('hiiiiiiiii')
+})
+
+
+app.listen(3000, () => {
+    console.log('express started on port 3000')
+});
